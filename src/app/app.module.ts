59dc69040fe702abbcd4568f8d363058231211a5
m.yamamoto <m.yamamoto@accelia.net>
2017-11-01 22:17:25 +0900

    ng2-bootstrap to ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
-import { Ng2BootstrapModule } from 'ng2-bootstrap';
+import { Ng2BootstrapModule } from 'ngx-bootstrap';
 
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { HeaderComponent } from './header/header.component';
