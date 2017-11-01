1515fd88396c8ebfd5f2a4a6a8592692b2e0289c
m.yamamoto <m.yamamoto@accelia.net>
2017-11-02 00:32:49 +0900

    ヘッダーの見た目作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { Ng2BootstrapModule } from 'ngx-bootstrap';
 
@@ -14,6 +17,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     Ng2BootstrapModule.forRoot()
   ],
   providers: [],
