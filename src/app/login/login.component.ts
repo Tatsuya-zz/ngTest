f4a1266974df21d37ca7822cdd8679fa714dc716
m.yamamoto <m.yamamoto@accelia.net>
2017-11-03 01:30:07 +0900

    メインコンテンツのCSSを修正

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+export class Person{
+  nickname: string;
+  passwd: string;
+}
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  person: Person = {
+    nickname: "",
+    passwd: ""
+  }
 }
